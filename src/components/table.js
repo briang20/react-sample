4f1f138ebc99b39241575a583a2fc892ecaae53e
Raven <7156279+RavenX8@users.noreply.github.com>
2020-01-28 11:02:12 -0500

    trying out kendo grids

---
+++ src/components/table.js
@@ -0,0 +1,23 @@
+// src/components/table.js
+
+import React from 'react';
+import { process } from '@progress/kendo-data-query';
+import { Grid, GridColumn } from '@progress/kendo-react-grid';
+
+
+const Table = ({ contacts }) => {
+    return (
+    <div className="usa-table">
+        <Grid 
+            data={contacts}
+            pageable={true}
+            sortable={true}>
+            <GridColumn field="name" />
+            <GridColumn field="email" />
+            <GridColumn field="catchPhrase" />
+        </Grid>
+    </div>
+    );
+}
+
+export default Table;
