3c1c1fd0228f458deb8aff8fb7b1ea2d7e96dd4e
Raven <7156279+RavenX8@users.noreply.github.com>
2020-02-11 16:18:20 -0500

    Updates to merge put requests into one API call

--- src/js/reducers/contacts.js
+++ src/js/reducers/contacts.js
@@ -26,13 +26,26 @@ export default function contacts(state = {contacts: [], replayBuffer: [], curren
                 replayBuffer: state.replayBuffer.concat({type: 'delete', data: action.payload})
             });
         case MODIFY_CONTACT:
+            const contact = state.replayBuffer.find(element => element.id === action.payload.id);
+            if (!contact) {
+
+            }
+
             return Object.assign({}, state, {
                 contacts: state.contacts.map(obj => {
-                    if (obj === action.oldPayload)
-                        return action.payload;
+                    if (obj === action.oldPayload) {
+                        let newData = Object.assign({}, obj);
+                        newData[action.payload.field] = action.payload.value;
+                        return newData;
+                    }
                     return obj;
                 }),
-                replayBuffer: state.replayBuffer.concat({type: 'put', data: action.payload})
+                replayBuffer: state.replayBuffer.map(event => {
+                    if (event.type === 'put' && event.data.id === action.payload.id) {
+
+                    }
+                    return event;
+                })
             });
         case CLEAR_CONTACTS:
             return Object.assign({}, state, {
